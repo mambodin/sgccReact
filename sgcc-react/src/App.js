836f73237f408ff58c5e526d5625355daffd120f
malik <mdmalik.b@gmail.com>
2019-08-16 02:27:02 +0800

    Second Commit

--- sgcc-react/src/App.js
+++ sgcc-react/src/App.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
 import './App.css';
 
-function App() {
+function App(props) {
+
+    const[count, setCount] = useState(props.count);
+    useEffect(()=>{
+      
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+    <div>
         <p>
           Hello world! 
           This is your SGCC React course
         </p>
-      </header>
+        Your count is {count}
+        <p>
+        <button onClick={()=> setCount(count + 1)}>+1</button>
+        <button onClick={()=> setCount(count - 1)}>-1</button>
+        <button onClick = {() => setCount(props.count)}> Reset </button>
+      </p>
     </div>
   );
+}
+
+App.defaultProps = {
+  count: 0
 }
 
 export default App;
