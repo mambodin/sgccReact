c21a4a83484cceedcf192b3d9b8d7bcab0fefaae
malik <mdmalik.b@gmail.com>
2019-08-16 09:35:41 +0800

    Nightly Build

--- sgcc-react/src/App.js
+++ sgcc-react/src/App.js
@@ -3,10 +3,12 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App(props) {
-
-    const[count, setCount] = useState(props.count);
+  
+    const countData = JSON.parse(localStorage.getItem('count'))
+    const[count, setCount] = useState(countData || 0);
     useEffect(()=>{
-      
+      localStorage.setItem('count', JSON.stringify(count))
+
     })
 
   return (
@@ -19,14 +21,14 @@ function App(props) {
         <p>
         <button onClick={()=> setCount(count + 1)}>+1</button>
         <button onClick={()=> setCount(count - 1)}>-1</button>
-        <button onClick = {() => setCount(props.count)}> Reset </button>
+        <button onClick = {() => setCount(0)}> Reset </button>
       </p>
     </div>
   );
 }
 
-App.defaultProps = {
-  count: 0
-}
+// App.defaultProps = {
+//   count: 0
+// }
 
 export default App;
