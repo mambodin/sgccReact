fa2d17358a1a7a5db7f8f6645dd3f674894ac7da
malik <mdmalik.b@gmail.com>
2019-08-16 09:47:52 +0800

    Morning commit

--- sgcc-react/src/App.js
+++ sgcc-react/src/App.js
@@ -1,31 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
 function App(props) {
-  
-    const countData = JSON.parse(localStorage.getItem('count'))
-    const[count, setCount] = useState(countData || 0);
-    useEffect(()=>{
-      localStorage.setItem('count', JSON.stringify(count))
 
-    })
+  const countData = JSON.parse(localStorage.getItem('count'))
+  const [count, setCount] = useState(countData || 0);
+  useEffect(() => {
+    localStorage.setItem('count', JSON.stringify(count))
+
+  })
 
   return (
     <div>
-        <p>
-          Hello world! 
-          This is your SGCC React course
+      <p>
+        Hello world!
+        This is your SGCC React course
         </p>
-        Your count is {count}
-        <p>
-        <button onClick={()=> setCount(count + 1)}>+1</button>
-        <button onClick={()=> setCount(count - 1)}>-1</button>
-<<<<<<< HEAD
-        <button onClick = {() => setCount(0)}> Reset </button>
-=======
-        <button onClick = {() => setCount(props.count)}> Reset Button</button>
->>>>>>> 09ce7df3e0247354bab5122c9af8181b26d680ea
+      Your count is {count}
+      <p>
+        <button onClick={() => setCount(count + 1)}>+1</button>
+        <button onClick={() => setCount(count - 1)}>-1</button>
+        <button onClick={() => setCount(0)}> Reset Button</button>
       </p>
     </div>
   );
