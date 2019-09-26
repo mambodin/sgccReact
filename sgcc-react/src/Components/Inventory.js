a59e2fc9a6c6893e85be8ca0d2586c93c23563ca
malik <mdmalik.b@gmail.com>
2019-09-27 01:09:36 +0800

    Added subscription

--- sgcc-react/src/Components/Inventory.js
+++ sgcc-react/src/Components/Inventory.js
@@ -2,38 +2,67 @@ import React, { useState, useEffect } from 'react';
 import firebase from './fire'
 
 
-const Inventory = () => {
+const DataPull = () => {
     let [data, setData] = useState([])
 
-    let readData = async () => {
-        let snapshot = await firebase.database().ref("items/").once("value");
-        let dataRef = snapshot.val();
-        let dataArray = Object.entries(dataRef)
-        // localStorage.setItem('items', JSON.stringify(dataRef))
-        setData(dataArray)
-    }
-
-
     useEffect(() => {
-        readData()
-        // let myData = readData()
-        // return () => readData()
+
+        // let firebasePull = async () => {
+        //     let snapshot = await firebase.database().ref("react-firebase/").once("value");
+        //     let dataRef = snapshot.val();
+        //     let dataArray = Object.entries(dataRef)
+        //     setData(dataArray)
+
+        // }
+        // firebasePull()
+
+        let fireBasePull = firebase.database().ref('react-firebase/').on("value", (snapshot) => {
+                let dataRef = snapshot.val()
+                let dataArray = Object.entries(dataRef)
+                setData(dataArray)
+            })
+        
+        return () => fireBasePull()
     }, [])
+
+    console.log(data)
+    return data
+}
+
+const DataSave = ({ name, price, image }) => {
+    useEffect(() => {
+        firebase.database().ref("react-firebase/" + name).set({
+            name,
+            price,
+            image
+        })
+    })
+}
+
+const Inventory = () => {
+
+    let items = DataPull()
+
+    DataSave({name:"malik",image:'http://',price:100})
+
 
     return (
         <div>
             <p>Clappity duck</p>
             {
-                data.map(d => {
+                items.map(d => {
                     return (
                         <div key={d[0]}>
-                        <p >{d[1].name}</p>
-                            <img src={ d[1].image } width ="200"/>
+                            <p >{d[1].name}</p>
+                            <img src={d[1].image} width="200" />
                         </div>
                     )
                 })
 
             }
+            <div>
+
+            </div>
 
         </div>
     )
