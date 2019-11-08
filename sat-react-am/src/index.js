269716328ba42da6289cfdc0ab15c9b4abf0ed87
malik <mdmalik.b@gmail.com>
2019-11-08 17:49:06 +0800

    8 Nov 19

--- sat-react-am/src/index.js
+++ sat-react-am/src/index.js
@@ -2,50 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import DisplaySuperhero from './DisplaySuperhero'
 import Title from './Title';
-
-
+import Counter from './Counter'
+import Age from './Age'
+import Calculator from './Calculator'
+import Notes from './Notes'
+import NoteApp from './NoteApp'
 let root = document.getElementById('root')
 
-let name = "Malik"
- 
-let dcComics =[ {
-    name:"Clarke Kent",
-    age: 37,
-    weight: 85,
-    img:"http://www.slashfilm.com/wp/wp-content/images/superman-action-comics-10000-e1520623702913.jpg"
-},
-{
-    name: "Bruce Wayne",
-    age: 37,
-    weight: 85,
-    img: "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fimages5.fanpop.com%2Fimage%2Fphotos%2F26800000%2FThe-Dark-Knight-Rises-bruce-wayne-26865596-800-736.jpg&f=1&nofb=1"
-}
-]
-
-const App = () => {
-    return (
-    <div>
-    <Title
-        title={name}
-    />
-       {
-           dcComics.map((elem)=> {
-          return <DisplaySuperhero
-               name={elem.name}
-               age={elem.age}
-               weight={elem.weight}
-               img={elem.img}
-               key={elem.name}
-           />
-       })
-       } 
-    </div>
-    )
-}
 
 
 
-ReactDOM.render(<App/>, root);
+
+
+
+ReactDOM.render(<NoteApp/>, root);
 
 
 
